de36c315f95cbfe8b706a763971a15ea9a21f297
ujkuo <ujkuo@ntu.im>
2022-10-13 15:03:13 +0800

    update automatic program description

--- academicStyle/auto.sh
+++ academicStyle/auto.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 git push
+cp cv.pdf ~/Desktop/Application/
+echo "***update CV to application folder***"
 cp cv.pdf ../../ujkuo.github.io/content/cv
-echo "move file to page"
+echo "***move file to page***"
 cd ../../ujkuo.github.io
 git add content/cv/cv.pdf
 git commit -m "update the latest CV"
 git push
-
+echo "***update personal website***"
