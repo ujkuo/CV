19f434f4b59c7bd0428f7fed2550d9c58b315f40
ujkuo <ujkuo@ntu.im>
2022-10-19 07:49:39 +0800

    update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,9 +3,9 @@
 git push
 cp cv.pdf ~/Desktop/Application/
 echo "***update CV to application folder***"
-cp cv.pdf ~/Document/ujkuo.github.io/content/cv
+cp cv.pdf ~/Documents/ujkuo.github.io/content/cv
 echo "***move file to page***"
-cd ~/Document/ujkuo.github.io
+cd ~/Documents/ujkuo.github.io
 git add content/cv/cv.pdf
 git commit -m "update the latest CV"
 git push
