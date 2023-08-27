49c60e71fe4c9bc09fcc3d9a4479405c6abbf715
ujkuo <ujkuo@ntu.im>
2023-08-27 23:12:42 +0800

    modify auto commit and update website bash

--- auto.sh
+++ auto.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-
+git aa 
+git commit -m "$1"
 git push
 cp cv.pdf ~/Desktop/Application/
+echo "====================================="
 echo "***update CV to application folder***"
+echo "====================================="
 cp cv.pdf ~/Documents/ujkuo.github.io/content/cv
+echo "======================="
 echo "***move file to page***"
+echo "======================="
 cd ~/Documents/ujkuo.github.io
 git add content/cv/cv.pdf
-git commit -m "update the latest CV"
+git commit -m "update the latest CV with the commit message: $1"
 git push
+echo "============================="
 echo "***update personal website***"
+echo "============================="
